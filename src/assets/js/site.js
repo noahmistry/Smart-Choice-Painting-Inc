186508bd78f74fecd7f873fc5572d886d5dabdb9
Noah Mistry <noah.mistry@gmail.com>
2020-05-14 13:10:06 -0700

    Animation and Logic for Scroll to top button

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,4 +1,28 @@
-    // $('#fancyBox .img-thumbnail').fancybox({
-    //     autoScale:true,
-    //     autoDimensions:true
-    // })
+// Scroll To Top Button 
+const topButton = document.getElementById('toTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 250) {
+        if (!topButton.classList.contains('animate__zoomIn')) {
+            topButton.classList.remove('animate__zoomOut');
+            topButton.classList.add('animate__zoomIn');
+            topButton.style.display = 'block';
+        }
+    } else {
+        if (topButton.classList.contains('animate__zoomIn')) {
+            topButton.classList.remove('animate__zoomIn');
+            topButton.classList.add('animate__zoomOut');
+            setTimeout(() => {
+                topButton.style.display = 'none';
+            }, 250);
+        }
+    }
+}) 
+topButton.addEventListener('click', (event) =>  {
+    event.preventDefault();
+    window.scroll({
+        top:0,
+        left:0,
+        behavior: 'smooth'
+    });
+});
